83a91f926e2a8105a21d573c5e9357d6366f4571
Jose Andres <jags9415@gmail.com>
2013-10-06 20:18:50 -0600

    Creacion del REST Service.

---
+++ LevelUpService/LevelUpService/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LevelUpService
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/Add?x={x}&y={y}")]
+        int Add(int x, int y);
+    }
+
+}
---
+++ LevelUpService/LevelUpService/Service.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.ServiceModel.Activation;
+
+namespace LevelUpService
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service : IService
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
