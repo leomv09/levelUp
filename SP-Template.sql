8337f7ea0e056c4d6c291e0ccd2ab8d94546fc23
Jose Andres <jags9415@gmail.com>
2013-10-15 21:50:19 -0600

    Agregar plantilla para stored procedures.

---
+++ SP-Template.sql
@@ -0,0 +1,54 @@
+﻿-----------------------------------------------------------
+-- Autor: Nombre o Usuario
+-- Fecha: 12/17/2011
+-- Descripcion: ----
+-- @ParameterName: Agregar explicacion de parametros complejos
+-----------------------------------------------------------
+--Autor: Nombre o Usuario
+--Fecha: 07/10/2012
+--Descripcion: Se describe una modificacion que sufrio el SP en el tiempo
+-----------------------------------------------------------
+CREATE PROCEDURE [dbo].[SENSP_ActualizarCliente]  --<Prefijo>SP_accionObjeto
+	@ParametroUno VARCHAR(18),
+	@ParametroDos NVARCHAR(30),
+	@ParametroN BIT=0
+AS	
+	SET NOCOUNT ON  -- apagar el envio de metadatos
+	
+	-- Variables para control de transaccion
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+
+	-- Realizar todas las operaciones de lectura posibles y precalculos que 
+	-- no sean parte o que puedan realizarse fuera de la transaccion
+	
+	
+	SET @InicieTransaccion = 0 -- control de transacciones
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION		
+	END			
+	
+	BEGIN TRY -- control de excepciones
+		-- aca mi codigo de transaccion	
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END		
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+		
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i', 
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH	
+	
+RETURN 0
