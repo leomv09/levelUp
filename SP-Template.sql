10a291270c13b8e9649313f98f05cba0531da882
Jose Andres <jags9415@gmail.com>
2013-10-16 21:14:34 -0600

    Establecer descripcion de regla NOT NULL

--- SP-Template.sql
+++ SP-Template.sql
@@ -2,39 +2,39 @@
 -- Autor: Nombre o Usuario
 -- Fecha: 12/17/2011
 -- Descripcion: ----
--- @ParameterName: Agregar explicacion de parametros complejos
+-- @ParameterName: Agregar explicacion de parametros complejos.
 -----------------------------------------------------------
 --Autor: Nombre o Usuario
 --Fecha: 07/10/2012
---Descripcion: Se describe una modificacion que sufrio el SP en el tiempo
+--Descripcion: Se describe una modificacion que sufrio el SP en el tiempo.
 -----------------------------------------------------------
 CREATE PROCEDURE [dbo].[SENSP_ActualizarCliente]  --<Prefijo>SP_accionObjeto
 	@ParametroUno VARCHAR(18),
 	@ParametroDos NVARCHAR(30),
-	@ParametroN BIT=0
+	@ParametroN BIT = D0
 AS	
-	SET NOCOUNT ON  -- apagar el envio de metadatos
+	SET NOCOUNT ON  -- Apagar el envio de metadatos.
 	
-	-- Variables para control de transaccion
+	-- Variables para control de transaccion.
 	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
 	DECLARE @Message VARCHAR(200)
 	DECLARE @InicieTransaccion BIT
 
 	-- Realizar todas las operaciones de lectura posibles y precalculos que 
-	-- no sean parte o que puedan realizarse fuera de la transaccion
+	-- no sean parte o que puedan realizarse fuera de la transaccion.
 	
 	
-	SET @InicieTransaccion = 0 -- control de transacciones
-	IF @@TRANCOUNT=0 BEGIN
+	SET @InicieTransaccion = 0 -- Control de transacciones.
+	IF @@TRANCOUNT = 0 BEGIN
 		SET @InicieTransaccion = 1
 		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
 		BEGIN TRANSACTION		
 	END			
 	
-	BEGIN TRY -- control de excepciones
-		-- aca mi codigo de transaccion	
+	BEGIN TRY -- Control de excepciones
+		-- Aca el codigo de la transaccion.
 
-		IF @InicieTransaccion=1 BEGIN
+		IF @InicieTransaccion = 1 BEGIN
 			COMMIT
 		END		
 	END TRY
@@ -44,7 +44,7 @@ AS
 		SET @ErrorState = ERROR_STATE()
 		SET @Message = ERROR_MESSAGE()
 		
-		IF @InicieTransaccion=1 BEGIN
+		IF @InicieTransaccion = 1 BEGIN
 			ROLLBACK
 		END
 		RAISERROR('%s - Error Number: %i', 
