0bccacc07d6f3cdb382ab33df5f3dcb0fefb4fbb
Emmanuel Murillo Sánchez <emmanuel-ms@hotmail.com>
2013-10-19 14:46:38 -0600

    Update usuarios

--- usuarios.sql
+++ usuarios.sql
@@ -1,3 +1,4 @@
+ï»¿USE LevelUp;
 DELETE FROM Usuario
 declare @userName varchar (70)
 DECLARE @nombre varchar(25)
@@ -8,9 +9,9 @@ set @count =0
 
 DECLARE @nombres table (name varchar (25))
 DECLARE @apellidos table (name varchar (25))
-INSERT INTO @nombres (name) VALUES ('Juan'), ('Pedro'), ('Pablo'), ('Alex'), ('Daniel'), ('David'), ('Alejandra'), ('Susan'), ('Tatiana'), ('María'), ('Cindy'), ('Alexa'), ('Jorge'), ('Alejandro'), ('Ariana'), ('Jose'), ('Eduardo'), ('Christina'), ('Emanuel'), ('Anthony'), ('Johanna'), ('Brenda'), ('Andrea'), ('Karla'), ('Luis'), ('Edgardo'), ('Denis'), ('Katherine'), ('Marta'), ('Marcia')
+INSERT INTO @nombres (name) VALUES ('Juan'), ('Pedro'), ('Pablo'), ('Alex'), ('Daniel'), ('David'), ('Alejandra'), ('Susan'), ('Tatiana'), ('MarÃ­a'), ('Cindy'), ('Alexa'), ('Jorge'), ('Alejandro'), ('Ariana'), ('Jose'), ('Eduardo'), ('Christina'), ('Emanuel'), ('Anthony'), ('Johanna'), ('Brenda'), ('Andrea'), ('Karla'), ('Luis'), ('Edgardo'), ('Denis'), ('Katherine'), ('Marta'), ('Marcia')
 
-INSERT INTO @apellidos (name) VALUES ('Chacon'), ('Lobo'), ('Fernandez'), ('Lopez'), ('Arias'), ('Cordero'), ('Salazar'), ('Campos'), ('Solis'), ('Vargas'), ('Cartin'), ('Morales'), ('Nuñez'), ('Rodriguez'), ('Delgado'), ('Alvarez'), ('mora'), ('Rojas'), ('Loria')
+INSERT INTO @apellidos (name) VALUES ('Chacon'), ('Lobo'), ('Fernandez'), ('Lopez'), ('Arias'), ('Cordero'), ('Salazar'), ('Campos'), ('Solis'), ('Vargas'), ('Cartin'), ('Morales'), ('NuÃ±ez'), ('Rodriguez'), ('Delgado'), ('Alvarez'), ('mora'), ('Rojas'), ('Loria')
 
 
 WHILE @count < 300
@@ -18,8 +19,8 @@ BEGIN
 SELECT TOP 1 @nombre = name FROM @nombres ORDER BY RAND()
 SELECT TOP 1 @apellido1 = name FROM @apellidos ORDER BY RAND()
 SELECT TOP 1 @apellido2 = name FROM @apellidos ORDER BY RAND()
-insert into usuario (Nombre, Apellido1, Apellido2, Contraseña, Username, fk_idEstadoUsuario)
-VALUES (@nombre, @apellido1, @apellido2, 'testContraseña', 'usuario' + cast (@count+1 as varchar), 1)
+insert into usuario (Nombre, Apellido1, Apellido2, [Contraseï¿½a], Username, fk_idEstadoUsuario)
+VALUES (@nombre, @apellido1, @apellido2, HashBytes('SHA1',@apellido1), @nombre + cast (@count+1 as varchar), 1)
 SET @count = @count+1
 END
 
